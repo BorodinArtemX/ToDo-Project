db157f194c64ed2e66f2cf8532db0d1001b4413c
BorodinArtemX <BorodinArtemX@icloud.com>
2023-01-22 18:08:39 +0100

    First commit

---
+++ MyToDoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoProject
+//
+//  Created by Артем Бородин on 21/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
