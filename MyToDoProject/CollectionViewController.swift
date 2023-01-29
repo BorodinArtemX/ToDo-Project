7984a07bd53f46e1b2d2768aa25d85b174e76c5f
BorodinArtemX <BorodinArtemX@icloud.com>
2023-01-29 09:47:54 +0100

    Adding Alert

--- MyToDoProject/CollectionViewController.swift
+++ MyToDoProject/CollectionViewController.swift
@@ -6,83 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
-private let reuseIdentifier = "Cell"
+    final class SomeCollectionViewCell: UICollectionViewCell {
+        
+        let someLabel: UILabel = {
+            let label = UILabel()
+            label.text = "   Some text"
+            return label
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            backgroundColor = .blue
+            setupLayout()
+        }
 
-class CollectionViewController: UICollectionViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private func setupLayout() {
+            addSubview(someLabel)
+            
+            someLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                someLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+                someLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+                someLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+            ])
+        }
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
-}
--- MyToDoProject/ViewController.swift
+++ MyToDoProject/ViewController.swift
@@ -7,74 +7,65 @@
 
 import UIKit
 import SnapKit
+
+final class ViewController: UIViewController {
     
-    final class ViewController: UIViewController {
-        private lazy var taskTableView: UITableView = {
-           let tableView = UITableView()
-            tableView.register(SomeCollectionView.self, forCellReuseIdentifier: "TaskTableViewCell")
-            tableView.dataSource = self
-            tableView.delegate = self
-            return tableView
-        }()
+ lazy var someCollectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .vertical
+    layout.minimumLineSpacing = 1
+    //layout.minimumInteritemSpacing = 15
+     layout.itemSize = CGSize(width: view.bounds.maxX , height: 50)
+    
+     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.register(SomeCollectionViewCell.self, forCellWithReuseIdentifier: "SomeCollectionViewCell")
+    collectionView.showsVerticalScrollIndicator = false
+    collectionView.delegate = self
+    collectionView.dataSource = self
+    return collectionView
+}()
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .white
-            navigationItem.title = "To Do"
-            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
-            setupLayout()
+override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setupLayout()
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
+}
+    @objc private func addNewTask() {
+        print("New task")
+        let alert = UIAlertController(title: "New task", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        let okAction = UIAlertAction(title: "OK", style: .default) { [unowned self] _ in
+            guard let task = alert.textFields!.first?.text else { return }
+            UserDefaults.standard.tasks.append(task)
+            self.someCollectionView.insertItems(at: [IndexPath(row: UserDefaults.standard.tasks.count - 1, section: 0)])
         }
-        
-        @objc private func addNewTask() {
-            print("New task")
-            let alert = UIAlertController(title: "New task", message: nil, preferredStyle: .alert)
-            alert.addTextField()
-            let okAction = UIAlertAction(title: "OK", style: .default) { [unowned self] _ in
-                guard let task = alert.textFields!.first?.text else { return }
-                UserDefaults.standard.tasks.append(task)
-                self.taskTableView.insertRows(at: [IndexPath(row: UserDefaults.standard.tasks.count - 1, section: 0)], with: .automatic)
-            }
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
-            alert.addAction(okAction)
-            alert.addAction(cancelAction)
-            present(alert, animated: true)
-        }
-        
-        private func setupLayout() {
-            view.addSubview(taskTableView)
-            
-            taskTableView.snp.makeConstraints { make in
-                make.top.bottom.equalTo(view.safeAreaLayoutGuide)
-                make.leading.trailing.equalToSuperview().inset(20)
-            }
-        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
+    
+private func setupLayout() {
+    view.addSubview(someCollectionView)
 
-    extension ViewController: UITableViewDelegate {
-        func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-            let action = UIContextualAction(style: .normal, title: "Delete") {  _,_,_ in
-
-                UserDefaults.standard.tasks.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .automatic)
-            }
-            action.backgroundColor = .red
-            let swipeConfig = UISwipeActionsConfiguration(actions: [action])
-            return swipeConfig
-        }
+    someCollectionView.snp.makeConstraints { make in
+        make.top.bottom.equalTo(view.safeAreaLayoutGuide)
+        make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(10)
     }
+}
+}
 
-    extension ViewController: UITableViewDataSource {
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            UserDefaults.standard.tasks.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell") as! SomeCollectionView
-            cell.setup(with: UserDefaults.standard.tasks[indexPath.row])
-            return cell
-        }
-        
-    }
+extension ViewController: UICollectionViewDataSource {
+func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+}
 
+func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SomeCollectionViewCell", for: indexPath) as! SomeCollectionViewCell
+    return cell
+}
+}
+
+extension ViewController: UICollectionViewDelegate {}
 
 
